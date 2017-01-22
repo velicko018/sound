1e4282866a1e801110774c8c77ac3c0dd61da084
velicko018 <velicko@elfak.rs>
2017-01-22 16:27:42 +0100

    Add AlbumSongsGroupViewModel

---
+++ MusicHouse/ViewModels/AlbumSongsGroupViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MusicHouse.Models;
+using Neo4jClient;
+
+namespace MusicHouse.ViewModels
+{
+    public class AlbumSongsGroupViewModel
+    {
+        public Node<Group> Group { get; set; }
+        public Node<Album> Albums { get; set; }
+
+        public IEnumerable<Song> Songs { get; set; }
+    }
+}
